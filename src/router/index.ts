1f9c4293b811c77967062fcb7ffe603f6548bc92
swpobread <jungeyo3@naver.com>
2026-06-27 19:23:46 +0900

    Feature: Add main page

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: () => import('@/components/HomeView.vue') },
+    { path: '/map', component: () => import('@/components/MapViewer.vue') },
+    { path: '/characters', component: () => import('@/components/MapViewer.vue') },
+    { path: '/scenarios', component: () => import('@/components/HomeView.vue') },
+  ],
+})
+export default router
